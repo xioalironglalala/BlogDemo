99fe6176f2fa0f4efff94a9aa60b3120eeb58354
xioalironglalala <xioalironglalala@163.com>
2024-05-23 01:05:41 +0800

    引入图表插件

---
+++ pages/echartsDemo/text.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="textInfo">
+		以下所有数据来自ChatGPT，仅供娱乐
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.textInfo {
+		margin: 6px 12px;
+		font-size: 14px;
+	}
+</style>
